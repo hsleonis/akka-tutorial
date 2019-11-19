05cd4999afb7a8a23bc514bcaea1ee7ee732570c
carlaterboven <carla.terboven@student.hpi.de>
2019-11-19 19:59:34 +0100

    Add hint cracking to worker

--- ddm-pc/src/main/java/de/hpi/ddm/actors/Master.java
+++ ddm-pc/src/main/java/de/hpi/ddm/actors/Master.java
@@ -4,12 +4,17 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import akka.actor.AbstractLoggingActor;
 import akka.actor.ActorRef;
 import akka.actor.PoisonPill;
 import akka.actor.Props;
 import akka.actor.Terminated;
+import akka.routing.ActorRefRoutee;
+import akka.routing.RoundRobinRoutingLogic;
+import akka.routing.Routee;
+import akka.routing.Router;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,6 +27,8 @@ public class Master extends AbstractLoggingActor {
 	
 	public static final String DEFAULT_NAME = "master";
 
+	Router router = new Router(new RoundRobinRoutingLogic());
+
 	public static Props props(final ActorRef reader, final ActorRef collector) {
 		return Props.create(Master.class, () -> new Master(reader, collector));
 	}
@@ -30,6 +37,7 @@ public class Master extends AbstractLoggingActor {
 		this.reader = reader;
 		this.collector = collector;
 		this.workers = new ArrayList<>();
+		this.routees = new ArrayList<Routee>();
 	}
 
 	////////////////////
@@ -51,6 +59,12 @@ public class Master extends AbstractLoggingActor {
 	public static class RegistrationMessage implements Serializable {
 		private static final long serialVersionUID = 3303081601659723997L;
 	}
+
+	@Data @NoArgsConstructor @AllArgsConstructor
+	public static class PasswordMessage implements Serializable {
+		private static final long serialVersionUID = -102767440935270949L;
+		private String result;
+	}
 	
 	/////////////////
 	// Actor State //
@@ -60,7 +74,11 @@ public class Master extends AbstractLoggingActor {
 	private final ActorRef collector;
 	private final List<ActorRef> workers;
 
+	private final ArrayList<Routee> routees;
+
 	private long startTime;
+	private int numPasswords;
+	private int numEncryptedPasswords;
 	
 	/////////////////////
 	// Actor Lifecycle //
@@ -82,6 +100,7 @@ public class Master extends AbstractLoggingActor {
 				.match(BatchMessage.class, this::handle)
 				.match(Terminated.class, this::handle)
 				.match(RegistrationMessage.class, this::handle)
+				.match(PasswordMessage.class, this::handle)
 				.matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
 				.build();
 	}
@@ -91,8 +110,17 @@ public class Master extends AbstractLoggingActor {
 		
 		this.reader.tell(new Reader.ReadMessage(), this.self());
 	}
+
+	protected void handle(PasswordMessage message) {
+		this.collector.tell(new Collector.CollectMessage(message.getResult()), this.self());
+		numEncryptedPasswords++;
+		if(numEncryptedPasswords == numPasswords){
+			this.collector.tell(new Collector.PrintMessage(), this.self());
+			this.terminate();
+		}
+	}
 	
-	protected void handle(BatchMessage message) {
+	protected void handle(BatchMessage message) throws InterruptedException {
 		
 		///////////////////////////////////////////////////////////////////////////////////////////////////////
 		// The input file is read in batches for two reasons: /////////////////////////////////////////////////
@@ -100,16 +128,32 @@ public class Master extends AbstractLoggingActor {
 		// 2. If we process the batches early, we can achieve latency hiding. /////////////////////////////////
 		// TODO: Implement the processing of the data for the concrete assignment. ////////////////////////////
 		///////////////////////////////////////////////////////////////////////////////////////////////////////
-		
+
 		if (message.getLines().isEmpty()) {
 			this.collector.tell(new Collector.PrintMessage(), this.self());
-			this.terminate();
 			return;
 		}
-		
-		for (String[] line : message.getLines())
-			System.out.println(Arrays.toString(line));
-		
+
+		String[] line = message.getLines().get(0);
+		//for (String[] line : message.getLines()) {
+			//System.out.println("password: " + line[4]);
+			String[] hints = Arrays.copyOfRange(line, 5, line.length);;
+
+			//for (String hint : hints) {
+				Worker.HintMessage hintMsg = new Worker.HintMessage();
+				hintMsg.setHints(hints);
+				hintMsg.setPasswordLength(Integer.parseInt(line[3]));
+				hintMsg.setPasswordChars(line[2]);
+				hintMsg.setSender(this.self());
+
+				router.route(hintMsg, this.self());
+			//}
+
+			//System.out.println(Arrays.toString(line));
+		//}
+		//numPasswords += message.getLines().size();
+		numPasswords++;
+
 		this.collector.tell(new Collector.CollectMessage("Processed batch of size " + message.getLines().size()), this.self());
 		this.reader.tell(new Reader.ReadMessage(), this.self());
 	}
@@ -132,12 +176,18 @@ public class Master extends AbstractLoggingActor {
 	protected void handle(RegistrationMessage message) {
 		this.context().watch(this.sender());
 		this.workers.add(this.sender());
-//		this.log().info("Registered {}", this.sender());
+
+		router = router.addRoutee(this.sender());
+
+		this.log().info("Registered {}", this.sender());
 	}
 	
 	protected void handle(Terminated message) {
 		this.context().unwatch(message.getActor());
 		this.workers.remove(message.getActor());
+
+		this.routees.remove(message.getActor());
+
 //		this.log().info("Unregistered {}", message.getActor());
 	}
 }
--- ddm-pc/src/main/java/de/hpi/ddm/actors/Worker.java
+++ ddm-pc/src/main/java/de/hpi/ddm/actors/Worker.java
@@ -1,8 +1,10 @@
 package de.hpi.ddm.actors;
 
+import java.io.Serializable;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.LinkedList;
 import java.util.List;
 
 import akka.actor.AbstractLoggingActor;
@@ -16,6 +18,9 @@ import akka.cluster.ClusterEvent.MemberUp;
 import akka.cluster.Member;
 import akka.cluster.MemberStatus;
 import de.hpi.ddm.MasterSystem;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 public class Worker extends AbstractLoggingActor {
 
@@ -37,12 +42,22 @@ public class Worker extends AbstractLoggingActor {
 	// Actor Messages //
 	////////////////////
 
+	@Data @NoArgsConstructor @AllArgsConstructor
+	public static class HintMessage implements Serializable {
+		private static final long serialVersionUID = 4057807743872319842L;
+		private String[] hints;
+		private int passwordLength;
+		private String passwordChars;
+		private ActorRef sender;
+	}
+
 	/////////////////
 	// Actor State //
 	/////////////////
 
 	private Member masterSystem;
 	private final Cluster cluster;
+	private LinkedList<String> encryptedHints = new LinkedList();
 	
 	/////////////////////
 	// Actor Lifecycle //
@@ -70,6 +85,7 @@ public class Worker extends AbstractLoggingActor {
 				.match(CurrentClusterState.class, this::handle)
 				.match(MemberUp.class, this::handle)
 				.match(MemberRemoved.class, this::handle)
+				.match(HintMessage.class, this::handle)
 				.matchAny(object -> this.log().info("Received unknown message: \"{}\"", object.toString()))
 				.build();
 	}
@@ -98,6 +114,82 @@ public class Worker extends AbstractLoggingActor {
 	private void handle(MemberRemoved message) {
 		if (this.masterSystem.equals(message.member()))
 			this.self().tell(PoisonPill.getInstance(), ActorRef.noSender());
+	}
+
+	private void handle(HintMessage message) {
+		System.out.println(message.getHints());
+
+		//generate sequences, hash them and compare with hints
+		//all letters that are left -> print all possible strings, hash them, compare with password
+		//return password in password message
+
+		//example: Generating all permutations of an array
+		String allChars = message.getPasswordChars();
+		List<char[]> possibleChars = new LinkedList();
+
+		//int j = 0;
+		for(int j = 0; j < allChars.length(); j++){
+			char[] arr  = new char[allChars.length() - 1];
+			for (int i = 0, k = 0; i < allChars.length(); i++) {
+				if (i == j) {
+					continue;
+				}
+				arr[k++] = allChars.charAt(i);
+			}
+			possibleChars.add(arr);
+		}
+
+		System.out.println("num hints: " + message.getHints().length);
+
+		//char[] passwortSet;
+
+		// in the possibleChars array the chars are missing in the order of the passwordChars attribute
+		for(int i = 0; i < possibleChars.size(); i++) {
+		//for(char[] array : possibleChars) {
+			char[] array = possibleChars.get(i);
+			List<String> sequences = new LinkedList<String>();
+			heapPermutation(array, array.length, array.length, sequences);
+			boolean foundHint = encryptHint(message.getHints(), sequences);
+			if(foundHint == false){
+				System.out.println(allChars.charAt(i) + " is missing!");
+			}
+		}
+
+
+
+		/*
+		//example: Generating all possible strings of length k
+		char[] set = {'a', 'b'};
+		int k = 3;
+		int n = set.length;
+		List<String> l = new LinkedList<String>();
+		printAllKLengthRec(set, "", n, k, l);
+		System.out.println(l);
+		 */
+
+		Master.PasswordMessage msg = new Master.PasswordMessage();
+		msg.setResult(message.getPasswordChars());
+		message.getSender().tell(msg, this.self());
+
+	}
+
+	private boolean encryptHint(String[] hints, List<String> sequences) {
+		for (String word : sequences) {
+			for (String hint : hints) {
+				if(hash(word).equals(hint)){
+					encryptedHints.add(word);
+					System.out.println(encryptedHints.size() + " -- " + this.self());
+					return true;
+					/* if(encryptedHints.size() == message.getHints().length) {
+						return;
+					}
+					break;
+
+					 */
+				}
+			}
+		}
+		return false;
 	}
 	
 	private String hash(String line) {
@@ -140,6 +232,27 @@ public class Worker extends AbstractLoggingActor {
 				a[i] = a[size - 1];
 				a[size - 1] = temp;
 			}
+		}
+	}
+
+	// Generating all possible strings of length k
+	// https://www.geeksforgeeks.org/print-all-combinations-of-given-length/
+	private void printAllKLengthRec(char[] set, String prefix, int n, int k, List<String> l)
+	{
+		// Base case: k is 0, store prefix
+		if (k == 0)
+		{
+			l.add(prefix);
+			return;
+		}
+
+		// One by one add all characters from set and recursively call for k equals to k-1
+		for (int i = 0; i < n; ++i)
+		{
+			// Next character of input added
+			String newPrefix = prefix + set[i];
+			// k is decreased, because we have added a new character
+			printAllKLengthRec(set, newPrefix, n, k - 1, l);
 		}
 	}
 }
